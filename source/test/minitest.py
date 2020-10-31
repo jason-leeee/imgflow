52bb301d44c2188d51725fd2290c883bedf7fdc1
ml6jason <jason.li@ml6.eu>
2020-11-01 00:12:40 +0100

    test passed with mockup resize

--- source/test/minitest.py
+++ source/test/minitest.py
@@ -1,9 +1,10 @@
-from ..core.imgio import fromDir
+from ..core.input import fromDir
+from ..core.transform import resize
 
 def test_load_data():
-    dataset = fromDir("data/CCPD2019", image_format=".jpg").execute()
-    dataset.summary()
-    print(dataset)
+    dataset = fromDir("data/CCPD2019", image_format=".jpg")
+    dataset = resize(width=1024, height=1024)(dataset)
+    print(dataset.execute())
 
 if __name__ == "__main__":
     test_load_data()
