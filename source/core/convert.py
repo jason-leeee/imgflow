480722743b436e74507d8284aaf7b312d63c9614
ml6jason <jason.li@ml6.eu>
2020-11-03 11:27:57 +0100

    Initial converting opertions

--- source/core/convert.py
+++ source/core/convert.py
@@ -7,7 +7,7 @@ from .operation import OpOneToOne
 RESIZE_INTERPOLATION = cv2.INTER_AREA
 
 
-class ImgTransformResize(OpOneToOne):
+class ImgConvertDetectionTFRecord(OpOneToOne):
     def process(self, img, width, height):
         print(img)
         img.img = cv2.resize(img.img, (width, height), interpolation=RESIZE_INTERPOLATION)
